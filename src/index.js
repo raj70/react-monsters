76720b460e1ea97f846f47a0b735078e56de223f
rajen shrestha <rajen.shrestha@outlook.com>
2025-08-07 19:00:11 +1000

    updated to latest packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
@@ -15,3 +15,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+/** to update the packages to latest packages
+ * npm install -g npm-check-updates
+ *  ncu -u
+ *  npm install
+ */
